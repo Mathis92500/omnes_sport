b2a133cfca7845667601c2efdf98ce65a9d40383
Larnod <monjiant.arnaud@gmail.com>
2022-05-29 19:21:37 +0200

    Création de compte

--- cours_collectif.php
+++ cours_collectif.php
@@ -35,7 +35,7 @@ $db_found = mysqli_select_db($db_handle, $database);
 
 if ($db_found) {
 
-    $sql = "SELECT * FROM coachs WHERE sport='Cours Collectif'";
+    $sql = "SELECT * FROM coachs WHERE sport='Cours Collectifs'";
     $result = mysqli_query($db_handle, $sql);
     
     echo "<table border=\"1\">";
